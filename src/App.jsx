bed2584c3b5226e36c50d2dcd46c13995408aa0f
Anas Hany <anashany219@gmail.com>
2024-09-05 23:46:25 +0300

    🌐 Implement new routing structure with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home.jsx";
+import Error from "./ui/Error.jsx";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart.jsx";
+import Order from "./features/order/Order.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/:orderId", element: <Order /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "*", element: <Error /> },
+]);
+
 function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
