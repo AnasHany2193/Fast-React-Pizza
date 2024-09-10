1796cff05f43c9cc84f53e15e47505caf86454dc
Anas Hany <anashany219@gmail.com>
2024-09-10 19:01:16 +0300

    🔄 Reuse styles by leveraging React components for consistency

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,18 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+
+  return to === "-1" ? (
+    <button onClick={() => navigate(-1)} className={className}>
+      {children}
+    </button>
+  ) : (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
