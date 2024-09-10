34c2ed62f2071f5b477180fdf71ef0e9ba0ffbb3
Anas Hany <anashany219@gmail.com>
2024-09-10 14:14:17 +0300

    🖼️ Build main app layout with header, footer, and page structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/:orderId", element: <Order /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "*", element: <Error /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "*", element: <Error /> },
+    ],
+  },
 ]);
 
 function App() {
