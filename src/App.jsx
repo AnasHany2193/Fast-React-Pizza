4ede109a5d324b515dbb97b79636c8d22d7316f9
Anas Hany <anashany219@gmail.com>
2024-09-10 15:07:43 +0300

    📦 Fetch orders data and integrate into the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./ui/Home.jsx";
 import Error from "./ui/Error.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import Order from "./features/order/Order.jsx";
-import Menu, { loader as MenuLoader } from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 
 const router = createBrowserRouter([
@@ -14,15 +14,20 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
       {
         path: "/menu",
         element: <Menu />,
-        loader: MenuLoader,
+        loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
