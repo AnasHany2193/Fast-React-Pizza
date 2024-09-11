12b0e665785b1ab881deef30047340b139e1bb7a
Anas Hany <anashany219@gmail.com>
2024-09-11 17:37:46 +0300

    🔄 Add functionality to update cart item quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -40,14 +40,16 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity++;
-      item.totalPrice += item.unitPrice;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       // payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity--;
-      item.totalPrice -= item.unitPrice;
+      item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
   },
 });
