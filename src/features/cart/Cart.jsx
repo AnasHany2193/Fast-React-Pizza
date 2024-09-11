00ddfb8ea9edb4aabadda025c38f95601c74dab0
Anas Hany <anashany219@gmail.com>
2024-09-11 16:17:05 +0300

    🗑️ Implement functionality to delete items from the cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,9 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import EmptyCart from "./EmptyCart";
 import Button from "../../ui/Button";
-import { clearCart, getCart } from "./cartSlice";
 import LinkButton from "../../ui/LinkButton";
 import { getUsername } from "../user/userSlice";
+import { clearCart, getCart } from "./cartSlice";
 
 function Cart() {
   const dispatch = useDispatch();
@@ -21,8 +21,8 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.map((item, i) => (
-          <CartItem item={item} key={i} />
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -60,6 +60,9 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrise = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export const {
   addItem,
   clearCart,
