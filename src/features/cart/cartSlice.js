d145532817bedeb40f35334bcf8309cf2d8fddd4
Anas Hany <anashany219@gmail.com>
2024-09-11 18:04:43 +0300

    🛒 Use the cart to process new orders

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -59,7 +59,7 @@ export const getCart = (state) => state.cart.cart;
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
-export const getTotalCartPrise = (state) =>
+export const getTotalCartprice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCurrentQuantityById = (id) => (state) =>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,9 +1,14 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 
+import store from "../../store";
 import Button from "../../ui/Button";
+import EmptyCart from "../cart/EmptyCart";
+import { getUsername } from "../user/userSlice";
+import { formatCurrency } from "../../utils/helpers";
 import { createOrder } from "../../services/apiRestaurant";
-import { useSelector } from "react-redux";
+import { clearCart, getCart, getTotalCartprice } from "../cart/cartSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -11,38 +16,19 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   const navigation = useNavigation();
   const formErrors = useActionData();
+  const username = useSelector(getUsername);
   const isSubmitting = navigation.state === "submitting";
-  const username = useSelector((state) => state.user.username);
 
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartprice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
@@ -93,8 +79,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
@@ -104,7 +90,9 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? "Placing order..." : "Order now"}
+            {isSubmitting
+              ? "Placing order..."
+              : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -119,7 +107,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
 
   const errors = {};
@@ -132,8 +120,9 @@ export async function action({ request }) {
   // If everything is okay, create new order and redirect
   const newOrder = await createOrder(order);
 
+  // Do not overuse
+  store.dispatch(clearCart());
   return redirect(`/order/${newOrder.id}`);
-  // return null;
 }
 
 export default CreateOrder;
