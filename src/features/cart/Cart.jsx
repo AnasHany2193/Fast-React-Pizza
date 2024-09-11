73dc2007e91435ac30fa311d9f687cd6426124d2
Anas Hany <anashany219@gmail.com>
2024-09-11 15:48:00 +0300

    🛍️ Build the cart page for displaying selected items

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,41 +1,24 @@
-import CartItem from "./CartItem";
-import Button from "../../ui/Button";
-import LinkButton from "../../ui/LinkButton";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import CartItem from "./CartItem";
+import EmptyCart from "./EmptyCart";
+import Button from "../../ui/Button";
+import { clearCart, getCart } from "./cartSlice";
+import LinkButton from "../../ui/LinkButton";
+import { getUsername } from "../user/userSlice";
 
 function Cart() {
-  const cart = fakeCart;
-  const username = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
+  const cart = useSelector(getCart);
+  const username = useSelector(getUsername);
 
-  return (
+  return !cart.length ? (
+    <EmptyCart />
+  ) : (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart,{username}</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item, i) => (
@@ -48,7 +31,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -39,5 +39,8 @@ const userSlice = createSlice({
   },
 });
 
+export const getUsername = (state) => state.user.username;
+
 export const { updateName } = userSlice.actions;
+
 export default userSlice.reducer;
